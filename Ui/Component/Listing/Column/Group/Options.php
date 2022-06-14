2b992e7b15c9f8bc41b81f825a351b49b0b2d82a
Code Slicer <ivanaugustobd@pm.me>
2022-06-14 09:44:43 -0300

    feat: group filter as dropdown

---
+++ Ui/Component/Listing/Column/Group/Options.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+/** Copyright © Discorgento. All rights reserved. */
+
+namespace Discorgento\Queue\Ui\Component\Listing\Column\Group;
+
+use Discorgento\Queue\Model\ResourceModel\Message\CollectionFactory as MessageCollectionFactory;
+use Magento\Framework\Data\OptionSourceInterface;
+
+class Options implements OptionSourceInterface
+{
+    /** @var MessageCollectionFactory */
+    private $messageCollectionFactory;
+
+    public function __construct(
+        MessageCollectionFactory $messageCollectionFactory
+    ) {
+        $this->messageCollectionFactory = $messageCollectionFactory;
+    }
+
+    /** @inheritDoc */
+    public function toOptionArray()
+    {
+        $messages = $this->messageCollectionFactory->create();
+        $messages->getSelect()->group('group');
+
+        $groups = array_map(
+            fn ($message) => $message->getGroup(),
+            $messages->getItems()
+        );
+
+        return array_map(
+            fn ($group) => ['label' => $group, 'value' => $group],
+            $groups
+        );
+    }
+}
